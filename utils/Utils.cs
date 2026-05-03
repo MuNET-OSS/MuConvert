54c62a49c2f52895ddee8b38447b21bd22bcf237
Starrah <starrah@foxmail.com>
2026-05-03 20:33:47 +0800

    [R] 提取出Utils.BarAndTick和Utils.Tick函数。

--- utils/Utils.cs
+++ utils/Utils.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+using System.Globalization;
 using System.Numerics;
 using System.Reflection;
 using Rationals;
@@ -37,6 +37,33 @@ public static class Utils
     internal static int TokenType(string str) => _simaiLexerMap[str];
     
     internal static bool IsModifier(int tokenType) => tokenType is L.MODIFIER or L.TAP_TO_STAR or L.STAR_TO_TAP or L.NO_STAR;
+
+    public static (int, int) BarAndTick(Rational time, int resolution, int extraTicks = 0)
+    {
+        var bar = time.WholePart;
+        var tick = (time.FractionPart * resolution).Round();
+        tick += extraTicks;
+        
+        while (tick >= resolution)
+        {
+            tick -= resolution;
+            bar++;
+        }
+        while (tick < 0)
+        {
+            tick += resolution;
+            bar--;
+        }
+
+        return ((int)bar, (int)tick);
+    }
+    
+    public static int Tick(Rational time, int resolution, int extraTicks = 0, int? min = null)
+    {
+        var r = (int)((time * resolution).Round() + extraTicks);
+        if (min != null && r < min) r = min.Value;
+        return r;
+    }
 }
 
 internal static class ExtensionUtils
@@ -61,6 +88,17 @@ internal static class ExtensionUtils
     {
         if (r < 0) throw new ArgumentOutOfRangeException(nameof(r));
         return r.WholePart + (r.FractionPart == 0 ? 0 : 1);
+    }
+    
+    private static readonly Rational _half = new(1, 2);
+    // 工作范围仅限非负数；舍入策略方面，使用与系统库Math.Round相同的“四舍六入五成双”算法。
+    public static BigInteger Round(this Rational r)
+    {
+        if (r < 0) throw new ArgumentOutOfRangeException(nameof(r));
+        var whole = r.WholePart;
+        var frac = r.FractionPart;
+        var shouldAdd = frac > _half || (frac == _half && whole % 2 == 1);
+        return whole + (shouldAdd ? 1 : 0);
     }
     
     public static Rational Sum(this IEnumerable<Rational> source)
