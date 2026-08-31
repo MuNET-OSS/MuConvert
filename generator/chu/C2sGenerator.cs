e34f6338ed93ae1c196eeb450221542b93b68285
Starrah <starrah@foxmail.com>
2026-08-31 20:04:32 +0800

    把C2sGenerator改好

--- generator/chu/C2sGenerator.cs
+++ generator/chu/C2sGenerator.cs
@@ -69,8 +69,8 @@ public class C2sGenerator : IGenerator<ChuChart>
         List<string> resultLines = [];
         foreach (var n in chart.Notes)
         {
-            var line = FormatNote(n, alerts);
-            if (line != null) resultLines.Add(line);
+            var lines = FormatNote(n, alerts);
+            resultLines.AddRange(lines);
         }
 
         foreach (var line in resultLines.Concat(slaLines).OrderBy(_lineOrder))
@@ -87,82 +87,103 @@ public class C2sGenerator : IGenerator<ChuChart>
         }
     }
 
-    private static string AirColorTag(ChuNote n, List<Alert> alerts)
-    {
-        if (C2sAllowedColors.Contains(n.Tag)) return n.Tag;
-        else
-        {
-            if (n.Tag != "") alerts.Add(new Alert(Alert.LEVEL.Warning, string.Format(Locale.C2SUnsupportedAirColor, "C2S Generator", n.Type, n.Tag), n.Time));
-            return "DEF";
-        }
-    }
-    private static string AirCrushColorTag(ChuNote n, List<Alert> alerts)
-    {
-        if (C2sAllowedCrushColors.Contains(n.Tag)) return n.Tag;
-        else
-        {
-            if (n.Tag != "") alerts.Add(new Alert(Alert.LEVEL.Warning, string.Format(Locale.C2SUnsupportedAirColor, "C2S Generator", n.Type, n.Tag), n.Time));
-            return "DEF";
-        }
-    }
-
-    private static string FLKTag(ChuNote n) => n.Tag is "L" or "R" ? n.Tag : "L";
-    
-    private string? FormatNote(ChuNote n, List<Alert> alerts)
+    private List<string> FormatNote(ChuNote n, List<Alert> alerts)
     {
         var (m, o) = Utils.BarAndTick(n.Time, RSL);
-        var durTicks = Utils.Tick(n.Duration, RSL);
-        if (IsChainContinueSegments(n))
-        { // 特殊地，对于slide的后续段：为了保证能接上，必须保证start tick接在前一个音符的endTime的后面，duration也采用end-start的方式计算durTicks。否则可能会，因为舍入的误差，造成没有办法接起来。
-            (m, o) = Utils.BarAndTick(n.Previous!.EndTime, RSL);
-            durTicks = Utils.Tick(n.EndTime, RSL) - Utils.Tick(n.Previous!.EndTime, RSL);
-        }
-        var result = n.Type switch
+        List<List<string>> results = [];
+        
+        if (n.Type is ChuNoteType.Tap or ChuNoteType.Flick or ChuNoteType.Mine)
         {
-            "TAP" or "CHR" => $"{n.Type}\t{m}\t{o}\t{n.Cell}\t{n.Width}",
-            "HLD" or "HXD" => $"{n.Type}\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{durTicks}",
-            "SLD" or "SLC" or "SXD" or "SXC" => $"{n.Type}\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{durTicks}\t{n.EndCell}\t{n.EndWidth}\tSLD",
-            "FLK" => $"FLK\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{FLKTag(n)}",
-            "AIR" or "AUR" or "AUL" or "ADW" or "ADR" or "ADL" =>
-                    $"{n.Type}\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{n.TargetNote}\t{AirColorTag(n, alerts)}",
-            "AHD" or "AHX" => $"{n.Type}\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{n.TargetNote}\t{durTicks}\t{AirColorTag(n, alerts)}",
-            "ASD" or "ASC" => FormatAsdAsc(n, m, o, durTicks, alerts),
-            "ALD" => FormatAld(n, m, o, durTicks, alerts),
-            "MNE" => $"MNE\t{m}\t{o}\t{n.Cell}\t{n.Width}",
-            _ => alert(),
-        };
-        if (result == null) return null;
-        if (n.Type is "CHR" or "HXD" or "SXD" or "SXC") result += $"\t{n.Tag}";
-
-        #region 分音符变速组 SLA 的处理
-        if (n.SpeedGroup != 0)
-        {
-            slaLines.Add($"SLA\t{m}\t{o}\t{n.Cell}\t{n.Width}\t1\t{n.SpeedGroup}");
-            if (durTicks > 0)
+            var name = n.Type switch
             {
-                var (endM, endO) = Utils.BarAndTick(n.EndTime, RSL);
-                var (endC, endW) = IsSlide(n) || IsAirSlide(n) || IsAirCrush(n) ? (n.EndCell, n.EndWidth) : (n.Cell, n.Width);
-                slaLines.Add($"SLA\t{endM}\t{endO}\t{endC}\t{endW}\t1\t{n.SpeedGroup}");
+                ChuNoteType.Tap => n.IsAir ? n.AirDirection.ToString()
+                    : (n.IsEx ? "CHR" : "TAP"),
+                ChuNoteType.Flick => "FLK",
+                ChuNoteType.Mine => "MNE",
+                _ => throw Utils.Fail(),
+            };
+            List<string> r = [name, m.ToString(), o.ToString(), n.Cell.ToString(), n.Width.ToString()];
+
+            if (n.Type == ChuNoteType.Tap && n.IsEx) r.Add(ExDirections_ToUgc[n.Ex!.Value]); // CHR
+            else if (n.Type == ChuNoteType.Flick) r.Add(n.Ex == ExDirection.RS ? "R" : "L"); // FLK
+            else if (n.IsAir)
+            { // AIR
+                var targetStr = AsC2sPreviousStr(n.TargetNote);
+                Utils.Assert(targetStr != null, "MuConvert内部错误：Air音符的TargetNote出现了不合法的类型！");
+                r.AddRange(targetStr!, AirColor_ToUgc(n));
+            }
+            
+            results.Add(r);
+            if (n.SpeedGroup != 0) // 分音符变速组 SLA 的处理
+                results.Add(["SLA", ..r[1..5], "1", n.SpeedGroup.ToString()]);
+        }
+        else
+        {
+            var name = (n.Type, n.IsAir) switch
+            {
+                (ChuNoteType.Hold, true) => n.IsEx ? "AHX" : "AHD",
+                (ChuNoteType.Hold, false) => n.IsEx ? "HXD" : "HLD",
+                (ChuNoteType.Slide, true) => "ASD",
+                (ChuNoteType.Slide, false) => n.IsEx ? "SXD" : "SLD",
+                (ChuNoteType.Crush, true) => "ALD",
+                _ => throw Utils.Fail(),
+            };
+            
+            var start = (n.Time, n.Cell, n.Width, n.Height);
+            foreach (var seg in n.Segments)
+            {
+                // 必备的：name bar tick cell width
+                var (sB, sT) = Utils.BarAndTick(start.Time, RSL);
+                List<string> r = [name, sB.ToString(), sT.ToString(), start.Cell.ToString(), start.Width.ToString()];
+                
+                // 前驱targetNote 或 crushInterval
+                if (NeedsTargetNote(n))
+                {
+                    var targetStr = AsC2sPreviousStr(n.TargetNote);
+                    Utils.Assert(targetStr != null, "MuConvert内部错误：Air音符的TargetNote出现了不合法的类型！");
+                    r.Add(targetStr!);
+                }
+                else if (n.Type == ChuNoteType.Crush)
+                    r.Add(Utils.Tick(n.CrushInterval ?? 100, RSL).ToString());
+
+                // Air且不是Air-Hold的情况，这里要加上height
+                if (n.IsAir && n.Type != ChuNoteType.Hold)
+                    r.Add($"{n.Height:F1}");
+                
+                // 持续时间
+                // 为了保持两段之间紧密相接，durTicks必须通过end和start的tick直接作差得到，不能把seg.Length直接转tick，
+                // 不然可能会由于舍入误差，导致差1tick接不上等情况。
+                var endTime = start.Time + seg.Length;
+                var durTicks = Utils.Tick(endTime, RSL) - Utils.Tick(start.Time, RSL);
+                r.Add(durTicks.ToString());
+
+                // 结束的 cell width [height]
+                if (n.Type is ChuNoteType.Slide or ChuNoteType.Crush)
+                {
+                    r.AddRange([seg.EndCell.ToString(), seg.EndWidth.ToString()]);
+                    if (n.IsAir) r.Add($"{seg.EndHeight:F1}");
+                }
+
+                // 颜色
+                if (n.IsAir) r.Add(n.Color.ToString());
+                else if (n.Type == ChuNoteType.Slide) r.Add("SLD"); // C2S 1.13版本以上，Slide在最后，还需要加一个”SLD“后缀
+                
+                // Ex音符的话，要加上Ex方向
+                if (n.IsEx) r.Add(ExDirections_ToUgc[n.Ex!.Value]);
+                
+                results.Add(r);
+                start = (endTime, seg.EndCell, seg.EndWidth, seg.EndHeight);
+                if (n.SpeedGroup != 0) // 分音符变速组 SLA 的处理
+                    results.Add(["SLA", ..r[1..5], "1", n.SpeedGroup.ToString()]);
+            }
+            if (n.SpeedGroup != 0)
+            { // 分音符变速组 SLA 的处理：参照PenguinTools的实现，要把最后一段的结束点，也加到SLA上
+                var (sB, sT) = Utils.BarAndTick(start.Time, RSL); // 这里的start.Time，其实是最后一段的endTime
+                List<string> sParts = [sB.ToString(), sT.ToString(), start.Cell.ToString(), start.Width.ToString()]; // start.Cell/Width，实际上也是最后一段的endCell/endWidth
+                results.Add(["SLA", ..sParts, "1", n.SpeedGroup.ToString()]);
             }
         }
-        #endregion
         
-        return result;
-
-        string? alert()
-        {
-            alerts.Add(new Alert(Alert.LEVEL.Warning, Locale.C2SUnknownNoteType, n.Time));
-            return null;
-        }
-    }
-
-    private static string FormatAsdAsc(ChuNote n, int m, int o, int durTicks, List<Alert> alerts)
-    {
-        return FormattableString.Invariant($"{n.Type}\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{n.TargetNote}\t{n.Height:F1}\t{durTicks}\t{n.EndCell}\t{n.EndWidth}\t{n.EndHeight:F1}\t{AirColorTag(n, alerts)}");
-    }
-
-    private static string FormatAld(ChuNote n, int m, int o, int durTicks, List<Alert> alerts)
-    {
-        return FormattableString.Invariant($"ALD\t{m}\t{o}\t{n.Cell}\t{n.Width}\t{Utils.Tick(n.CrushInterval, RSL)}\t{n.Height:F1}\t{durTicks}\t{n.EndCell}\t{n.EndWidth}\t{n.EndHeight:F1}\t{AirCrushColorTag(n, alerts)}");
+        return results.Select(x=>string.Join('\t', x)).ToList();
     }
 }
